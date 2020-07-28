c73ca2085a0dbb8696a0f9a17b30ce4bd6f3ef99
Maxime Berthier <berthiermaxime@orange.fr>
2020-07-28 20:20:22 +0200

    fixed validation on review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to cocktail_path(@cocktail)
     else
+      @dose = Dose.new
       render 'cocktails/show'
     end
   end
