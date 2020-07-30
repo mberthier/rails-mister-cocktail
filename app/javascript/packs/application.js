8a0e526c2dbe34219dd3a69f32f544088c2c249d
Maxime Berthier <berthiermaxime@orange.fr>
2020-07-30 13:10:47 +0200

    added dynamic stars in the form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,17 @@ require("channels")
 
 import 'bootstrap';
 
+import { initStarRating } from '../plugins/init_star_rating';
+
+
+
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  initStarRating();
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
